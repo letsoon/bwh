be3a66dc1fccf2415d6fc848336ef8be918976e2
letsoon <laishihong@live.cn>
2021-12-30 19:05:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const domain = `https://api.64clouds.com/v1/getServiceInfo`;
 exec(`curl -d "api_key=${API_KEY}&veid=${VEID}" ${domain}`, (error, std) => {
     let desp = "";
     const res = JSON.parse(std);
-    const today = new Date().toLocaleString('zh-CN');
+    const today = new Date().toLocaleString('zh-CN',{
+        timeZone: 'Asia/Shanghai',
+        hour12: false,
+    });
     const data = res.plan_monthly_data / 1024 / 1024 / 1024 * res.monthly_data_multiplier;
     const usage = res.data_counter / 1024 / 1024 / 1024 * res.monthly_data_multiplier;
     const reset = data - usage;
